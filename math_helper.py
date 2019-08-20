60e3951060932b549affaf7acd540a8c429b1335
Chad <cpurdy@flourbluffschools.net>
2019-08-20 13:19:17 -0500

    update starter code

--- math_helper.py
+++ math_helper.py
@@ -6,11 +6,3 @@ Description: A program to help me out later this year in my
 """
 
 
-def main():
-    print("""This program doesn't do anything yet.
-It will eventually give the user a list of five
-options to do some math calculations!""")
-    
-
-if __name__ == "__main__":
-    main()
