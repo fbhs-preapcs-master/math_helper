205da15e31ad251d1e03bc7b53cd2041ce43e69f
Chad <cpurdy@flourbluffschools.net>
2019-08-20 10:18:02 -0500

    add math_helper starter code

---
+++ math_helper.py
@@ -0,0 +1,16 @@
+"""
+File: math_helper.py
+Name:  Your name      <--- Change this
+Description: A program to help me out later this year in my
+             math class
+"""
+
+
+def main():
+    print("""This program doesn't do anything yet.
+It will eventually give the user a list of five
+options to do some math calculations!""")
+    
+
+if __name__ == "__main__":
+    main()
